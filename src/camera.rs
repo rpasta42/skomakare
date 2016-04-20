29dd3c7f1bdb2a3fedd6e1f88a4bc48adad109e6
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-04-19 20:58:13 -0400

    Working on camera.

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,31 @@
-
-pub struct Camera {}
-impl Camera {}
+pub struct Camera {
+   pos : Point,
+   rot : Coord,
+   size : Point //% of parent width and height
+}
+impl Camera {
+   pub fn new() -> Camera {
+      Camera {
+         pos: [0.0, 0.0], rot : 0.0, size : [0.0, 0.0]
+      }
+   }
+   pub fn set_position(&mut self, pos_ : Point) {
+      self.pos = pos_;
+   }
+   pub fn move_(&mut self, pos_ : Point) {
+      //self.pos[0] = self.pos + pos_[0]
+   }
+   pub fn set_rotation(&mut self, rot_ : Coord) {
+      self.rot = rot_;
+   }
+   pub fn rotate(&mut self, rot_ : Coord) {
+      self.rot = rot_;
+   }
+   pub fn set_size(&mut self, size_ : Point) {
+      self.size = size_;
+   }
+   pub fn scale(&mut self, size_ : Point) {
+      self.size = size_;
+   }
+}
 
--- src/scene.rs
+++ src/scene.rs
@@ -11,54 +11,21 @@ use camera::*;
    fn resize(scale_factor : Coord);
 }*/
 
-pub struct ObjectData {
-   position : Option<Point>,
-   rotation : Option<Coord>,
-   size : Option<Point> //% of parent width and height
-}
-impl ObjectData {
-   fn new() -> ObjectData {
-      ObjectData {
-         position: None, rotation : None, size : None
-      }
-   }
-   pub fn position(&mut self, position_ : Point) -> &mut ObjectData {
-      self.position = Some(position_); self
-   }
-   pub fn rotation(&mut self, rotation_ : Coord) -> &mut ObjectData {
-      self.rotation = Some(rotation_); self
-   }
-   pub fn size(&mut self, size_ : Point) -> &mut ObjectData {
-      self.size = Some(size_); self
-   }
-   pub fn finalize(&self) -> ObjectData {
-      //calculate missing stuff and make sure we didn't get too many arguments
-      ObjectData {
-         position: self.position, rotation: self.rotation, size : self.size
-      }
-   }
+pub enum GameObjectType {
+   Model(Model), Scene(Scene), //Camera(Camera) //Physics(u32), Undecided
 }
 
-pub enum ObjectType {
-   Model(Model), Scene(Scene), Camera(Camera) //Physics(u32), Undecided
-}
 pub struct GameObject {
-   pub object_type : ObjectType,
-   pub object_data : ObjectData
+   pub data : GameObjectType,
+   pub cam  : Camera
 }
 impl GameObject {
-   pub fn new(object_type_ : ObjectType) -> GameObject {
+   pub fn new(object_type : GameObjectType) -> GameObject {
       GameObject {
-         object_type : object_type_, //ObjectType::Undecided,
-         object_data : ObjectData::new()
+         data : object_type, //ObjectType::Undecided,
+         cam  : Camera::new()
       }
    }
-   pub fn set_position(x : Point) {}
-   pub fn move_(x : Point) {}
-   pub fn set_rotation(x : Coord) {}
-   pub fn rotate(degrees : Coord) {}
-   pub fn set_size(scale : Coord) {}
-   pub fn resize(scale_factor : Coord) {}
 }
 
 pub struct Scene {
